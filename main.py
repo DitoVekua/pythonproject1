fadf1bd0012a1d32dfc318782a2c0480c1680376
DitoVekua <vekuadito@gmail.com>
2023-03-12 19:55:05 +0400

    NewProject

---
+++ main.py
@@ -0,0 +1,9 @@
+from models.car import Car
+
+car0 = Car("Subaru", "Impreza", 2002, "Blue", 300, "is_sport_car")
+car1 = Car("Mercedes", "G Class", 2022, "Black", 200, )
+
+print(car0.change_color("Black"))
+print(car0.increase_horse_power(50))
+
+
